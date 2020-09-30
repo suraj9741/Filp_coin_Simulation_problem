e4dac376a4631f81b023efa05993a9c589ae4046
Suraj <surajjadhav9741@gmail.com>
2020-09-30 20:06:07 +0530

    Add : Loop through Multiple times of flipping a coin store the Singlet combination in Dictionary and Find percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,12 +1,39 @@
 ##!/bin/bash -x
 echo "-----------------------Flip coin Combination-------------------------"
+Head=0
+Tail=0
+loop=0
+loops=21
+declare -A coin
+declare -A scoin
 coin_flip(){
 	n=$((RANDOM%2))
 	if [ $n -eq 1 ]
 	then
-		echo "Head"
+		Head=$((Head+1))
+		coin[H]=$Head
+		scoin[$1]=H
 	else
-		echo "Tail"
+		Tail=$((Tail+1))
+		coin[T]=$Tail
+		scoin[$1]=T
 	fi
 }
-coin_flip
+persentage(){
+	per=$(awk 'BEGIN { print '$1' / '$2' * '100'}')
+}
+while [ $loop -lt $loops ]
+do
+	coin_flip $loop
+	loop=$((loop+1))
+done
+echo "Heades = $Head & Tails = $Tail Out of = $loops"
+echo "${scoin[@]}"
+for i in "${!coin[@]}"
+do
+        echo "$i : ${coin[$i]}"
+done
+persentage $Head $loops
+echo "Head = $per %"
+persentage $Tail $loops
+echo "Tail = $per %"
