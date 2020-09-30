1e47a519d9af2b54de1c8d220443f965a64d756b
Suraj <surajjadhav9741@gmail.com>
2020-09-30 16:01:05 +0530

    Add : Function of coin Flipping and Display the result

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,2 +1,12 @@
-#!/bin/bash -x
+##!/bin/bash -x
 echo "-----------------------Flip coin Combination-------------------------"
+coin_flip(){
+	n=$((RANDOM%2))
+	if [ $n -eq 1 ]
+	then
+		echo "Head"
+	else
+		echo "Tail"
+	fi
+}
+coin_flip
